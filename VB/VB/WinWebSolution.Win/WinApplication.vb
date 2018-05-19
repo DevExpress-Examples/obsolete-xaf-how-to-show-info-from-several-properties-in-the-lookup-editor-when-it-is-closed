2b95f90fe34d7f3e0df02b6f7c312ef81166de9c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/VB/WinWebSolution.Win/WinApplication.vb
@@ -0,0 +1,32 @@
+﻿' Developer Express Code Central Example:
+' How to show info from several properties in the lookup editor when it is closed
+' 
+' This example is an intermediate workaround for the suggestion. To see how this
+' works, open the Order detail view and notice that the Customer editor displays
+' more information than usual.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E1322
+
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.Win
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.ExpressApp
+
+Namespace WinWebSolution.Win
+	Partial Public Class WinWebSolutionWindowsFormsApplication
+		Inherits WinApplication
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub WinWebSolutionWindowsFormsApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs) Handles MyBase.DatabaseVersionMismatch
+			e.Updater.Update()
+			e.Handled = True
+		End Sub
+	End Class
+End Namespace
